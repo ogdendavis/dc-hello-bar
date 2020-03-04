b1117b271dfb284a5a46c6c73432fbaaac95427e
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-03-04 14:43:59 -0500

    Add ad post type

--- index.php
+++ index.php
@@ -15,9 +15,20 @@ function dc_register_session() {
     $_SESSION['hello_bar'] = True;
   }
 }
-add_action('init','dc_register_session', 1);
+add_action( 'init', 'dc_register_session', 1 );
+
+function dc_add_post_types() {
+  register_post_type( 'dc_hello_ad',
+    array(
+      'labels' => array('name' => __('Hello Bar Ads'), 'singular_name' => __('Hello Bar Ad')),
+      'public' => true,
+    )
+  );
+}
+add_action( 'init', 'dc_add_post_types' );
 
 function dc_include_hello_bar() {
+  // Function used in templates to add the hello bar
   if ($_SESSION['hello_bar'] == True) {
     include 'src/template.php';
   }
